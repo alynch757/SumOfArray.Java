0a55745f7e744b4251889d4f75bd87b36f5aab2a
alynch757 <79422790+alynch757@users.noreply.github.com>
2021-03-15 23:09:16 -0400

    Add files via upload

---
+++ SumofArray.java
@@ -0,0 +1,26 @@
+
+public class SumofArray
+public static void main (String[] args) {
+	
+	// we declare the interger array 
+	int[] arr = new int[6];
+	
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
+	arr[3] = 4;
+	arr[4] = 5;
+	arr[5] = 6;
+	
+	// sum of these numbers
+	int sum = 0;
+	int lengthofArray = arr.length;
+	
+	for (int i = 0; i <lengthofArray; i ++) {
+		
+		System.out.printIn(index);
+	}
+
+
+
+}
